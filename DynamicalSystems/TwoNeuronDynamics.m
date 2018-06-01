b397636bd29b4eef486ca91e6bfc55fffa8b2779
Seth Egger <swegger@mit.edu>
2018-06-01 15:11:52 -0400

    Final version of Two neuron model for TICs

--- DynamicalSystems/TwoNeuronDynamics.m
+++ DynamicalSystems/TwoNeuronDynamics.m
@@ -7,6 +7,7 @@ function TwoNeuronDynamics()
 %
 %%
 
+threshold = 0.4;
 u0s = [0.6 0.6 0.65 0.65];
 v0s = [0.65 0.65 0.6 0.6];
 I0s = [0.6 0.8 0.6 0.8];
@@ -16,7 +17,8 @@ colors = [218 112 146;
           218 112 146;
            65 185 235]/255;
 
-figure('Name','Two neuron','Position',[276 369 1231 495])
+h1 = figure('Name','Two neuron','Position',[204 160 528 918]);
+h2 = figure('Name','Two neuron','Position',[204 160 999 918]);
 for i = 1:length(I0s)
     %% Parameters
     u0 = u0s(i);
@@ -29,7 +31,7 @@ for i = 1:length(I0s)
     
     niter = 1001;
     
-    vvals = linspace(0,1,100);
+    vvals = linspace(0,1.2,100);
     
     %% Run simulation
     [u,v] = simulate_u_v(u0,v0,params,niter);
@@ -39,36 +41,45 @@ for i = 1:length(I0s)
     uvals = find_null_clines(vvals,params);
     
     %% Plot
-    subplot(1,2,1)
-    plot(u-v,'Color',colors(i,:))
-    hold on
+    %subplot(3,3,[1 4 7])
+    figure(h1)
+%     if i >= 3
+        temp = u-v;
+        plot(temp(abs(temp) < threshold),'Color',colors(i,:))
+        hold on
+%     end
 %     plot(v,'--','Color',colors(i,:))
     
-    subplot(1,2,2)
-    plot(u,v,'-','Color',colors(i,:))
+    figure(h2)
+    if i < 3
+        subplot(1,2,1)
+    else
+        subplot(1,2,2)
+    end
+%    subplot(ceil(sqrt(length(I0s))),ceil(sqrt(length(I0s))),i)
+%     plot(u,v,'-','Color',colors(i,:))
     hold on
-    plot(u(1:100:end),v(1:100:end),'.','Color',colors(i,:))
-    plot(u(1),v(1),'ko','MarkerFaceColor','k','MarkerSize',7,'Color',colors(i,:))
-    plot(u(end),v(end),'ks','MarkerFaceColor','k','MarkerSize',7,'Color',colors(i,:))
+    plot(u(1:50:end),v(1:50:end),'.','Color',colors(i,:))
+    plot(u(1),v(1),'ko','MarkerFaceColor',[1 1 1],'MarkerSize',7,'Color',colors(i,:))
+    plot(u(end),v(end),'ko','MarkerFaceColor',colors(i,:),'MarkerSize',7,'Color','k')
     
     plot(vvals,uvals(:,1),'Color',colors(i,:))
     plot(uvals(:,2),vvals,'--','Color',colors(i,:))
+    
     axis([0 1.2 0 1.2])
     axis square
+    plotUnity;
+    xlabel('u')
+    ylabel('v')
+    mymakeaxis(gca)
+
 
 end
 
-%% Clean up
-subplot(1,2,1)
-xlabel('t')
-ylabel('u-v')
-mymakeaxis(gca)
-
-subplot(1,2,2)
-plotUnity;
-xlabel('u')
-ylabel('v')
-mymakeaxis(gca)
+figure(h1)
+xlabel('Time')
+ylabel('x_1-x_2')
+mymakeaxis(gca,'yticks',[-1 0 1])
 
 %% Functions
 
@@ -113,7 +124,7 @@ function uvals = find_null_clines(vvals,params)
     I0 = params.I0;
     epsi = params.epsi;
     
-    uvals(:,1) = 1 ./ (1 + exp(-I0 + w*vvals - epsi) );
+    uvals(:,1) = 1 ./ (1 + exp(-I0 + w*vvals ) );
     uvals(:,2) = 1 ./ (1 + exp(-I0 + w*vvals ) );
     
     
